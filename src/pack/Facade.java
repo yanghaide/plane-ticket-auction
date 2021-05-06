bfb5583f96e2276ddf2351644e420757db91674d
Doryan Dahon <39133219+ddahon@users.noreply.github.com>
2021-05-06 09:10:35 +0200

    Web page structure

---
+++ src/pack/Facade.java
@@ -0,0 +1,11 @@
+package pack;
+
+import java.util.Collection;
+import java.util.Hashtable;
+
+import javax.ejb.Singleton;
+
+@Singleton
+public class Facade {
+	
+}
---
+++ src/pack/Serv.java
@@ -0,0 +1,52 @@
+package pack;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Serv")
+public class Serv extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	@EJB
+	private Facade facade;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Serv() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String op = request.getParameter("op");
+
+		if (op != null) {
+			// call the methods corresponding to op
+		} else {
+			request.getRequestDispatcher("index.jsp").forward(request,response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
